929fbbbbf3bd1d6959b6a8803701f28aebf09bdf
Tabbytech <tabbyccherop@gmail.com>
2025-07-02 00:36:57 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,5 @@
 --before index creation
-EXPLAIN ANALYZE select u.user_id,u.first_name, COUNT(b.booking_id)AS total_bookings FROM user u LEFT JOIN booking b ON u.user_id = b.user_id GROUP BY  u.user_id,u.first_name
+'EXPLAIN ANALYZE' select u.user_id,u.first_name, COUNT(b.booking_id)AS total_bookings FROM user u LEFT JOIN booking b ON u.user_id = b.user_id GROUP BY  u.user_id,u.first_name
 ORDER BY u.user_id;
 --most columns on user table
 --user_id as primary key index is AUTOMATICALLY created
@@ -9,7 +9,7 @@ CREATE INDEX idx_user_first_name ON user(first_name);
 CREATE INDEX idx_property_name ON property(name)
 -
 --- Property after creation
-EXPLAIN ANALYZE SELECT u.user_id, u.first_name, COUNT(b.booking_id) 
+'EXPLAIN ANALYZE' SELECT u.user_id, u.first_name, COUNT(b.booking_id) 
 AS total_bookings FROM  user   u LEFT JOIN booking b
 ON u.user_id= b.user_id
 GROUP BY u.user_id, u.first_name
