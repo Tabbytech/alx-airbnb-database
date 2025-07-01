646621fbc280d3a99df5cd7cca2d4002c13783a2
Tabbytech <tabbyccherop@gmail.com>
2025-07-01 21:46:40 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,20 @@
+--all properties where the average rating is greater than 4.0 using a subquery.
+SELECT p.property_id, p.name, p.location
+FROM Property AS p
+WHERE  p.property_id IN (
+ SELECT  r.property_id
+ FROM Review AS r
+ GROUP BY  r.property_id
+ HAVING  AVG(r.rating) > 4.0
+   );
+
+-- users who have made more than 3 bookings.
+SELECT u.user_id, u.first_name, u.last_name, u.email
+FROM User AS u
+WHERE (
+ SELECT COUNT(b.booking_id)
+ FROM Booking AS b
+WHERE  b.user_id = u.user_id ) > 3
+ORDER BY u.user_id;
+     
+ 
