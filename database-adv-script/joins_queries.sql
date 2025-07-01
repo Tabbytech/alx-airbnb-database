aad22cf30c1d55ac58c2e5c90eaf223842dd4e80
Tabbytech <tabbyccherop@gmail.com>
2025-07-01 21:17:26 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,17 @@
+--all bookings and the respective users who made those bookings.
+--INNER JOIN
+SELECT * FROM booking
+INNER JOIN User 
+  ON booking.user_id = user.user_id;
+
+--all properties and their reviews, including properties that have no reviews.
+--LEFT JOIN
+ SELECT * FROM Property 
+  LEFT JOIN Review 
+  ON property.property_id = review.property_id;
+
+--all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+--FULL OUTER JOIN
+SELECT * FROM User 
+FULL OUTER JOIN Booking 
+  ON user.user_id = booking.user_id;
