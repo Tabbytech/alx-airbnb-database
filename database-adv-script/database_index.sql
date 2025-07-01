d0285fa39b4f655735a8bec42f268082d70aaf1c
Tabbytech <tabbyccherop@gmail.com>
2025-07-02 00:14:27 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,31 +1,16 @@
--- Index on Property.host_id
--- Improves performance for queries filtering properties by host or joining with the User table on host_id.
-CREATE INDEX idx_property_host_id ON Property (host_id);
-
--- Index on Booking.user_id
--- Speeds up queries retrieving bookings made by a specific user or joining with the User table on user_id.
-CREATE INDEX idx_booking_user_id ON Booking (user_id);
-
--- Index on Booking.status
--- Enhances performance for queries filtering bookings based on their status (e.g., 'pending', 'confirmed').
-CREATE INDEX idx_booking_status ON Booking (status);
-
--- Index on Booking.start_date
--- Crucial for efficient date range queries and sorting bookings by their start date.
-CREATE INDEX idx_booking_start_date ON Booking (start_date);
-
--- Index on Review.user_id
--- Improves performance for queries retrieving reviews made by a specific user.
-CREATE INDEX idx_review_user_id ON Review (user_id);
-
--- Index on Review.rating
--- Useful for queries filtering reviews by rating or aggregating ratings (e.g., calculating average rating for a property).
-CREATE INDEX idx_review_rating ON Review (rating);
-
--- Index on Message.sender_id
--- Accelerates retrieval of messages sent by a particular user.
-CREATE INDEX idx_message_sender_id ON Message (sender_id);
-
--- Index on Message.recipient_id
--- Accelerates retrieval of messages received by a particular user.
-CREATE INDEX idx_message_recipient_id ON Message (recipient_id);
+--before index creation
+EXPLAIN ANALYZE select u.user_id,u.first_name, COUNT(b.booking_id)AS total_bookings FROM user u LEFT JOIN booking b ON u.user_id = b.user_id GROUP BY  u.user_id,u.first_name
+ORDER BY u.user_id;
+--most columns on user table
+--user_id as primary key index is AUTOMATICALLY created
+CREATE INDEX idx_user_first_name ON user(first_name);
+--most columns on property table
+---property_id as PK index is automaticallycreated
+CREATE INDEX idx_property_name ON property(name)
+-
+--- Property after creation
+EXPLAIN ANALYZE SELECT u.user_id, u.first_name, COUNT(b.booking_id) 
+AS total_bookings FROM  user   u LEFT JOIN booking b
+ON u.user_id= b.user_id
+GROUP BY u.user_id, u.first_name
+ORDER BY u.user_id;
